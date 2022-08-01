77f26004f05a3782ce1053b10fc7995b86f9878f
820471261@qq.com <820471261@qq.com>
2022-08-01 10:27:36 +0800

    新增scss 路由文件

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,13 +2,13 @@
  * @Author: liuyichen
  * @Date: 2022-07-29 14:46:15
  * @LastEditors: liuyichen
- * @LastEditTime: 2022-08-01 08:59:08
+ * @LastEditTime: 2022-08-01 10:03:06
  * @FilePath: \代码仓库\shop_dev_react\src\pages\home\index.tsx
  * @Description: 
  * 
  * Copyright (c) 2022 by liuyichen, All Rights Reserved. 
  */
-import styles from '../index.module.scss'
+import styles from './index.module.scss'
 const Home = ()=>{
   return <div className={styles.home}>home</div>
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,7 +2,7 @@
  * @Author: liuyichen
  * @Date: 2022-08-01 08:42:40
  * @LastEditors: liuyichen
- * @LastEditTime: 2022-08-01 08:48:37
+ * @LastEditTime: 2022-08-01 10:19:02
  * @FilePath: \代码仓库\shop_dev_react\src\pages\login\index.tsx
  * @Description: 
  * 
@@ -10,6 +10,7 @@
  */
 import { Button, Checkbox, Form, Input } from 'antd';
 import React from 'react';
+import styles from './index.module.scss'
 const Login: React.FC = () => {
   const onFinish = (values: any) => {
     console.log('Success:', values);
@@ -20,41 +21,44 @@ const Login: React.FC = () => {
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
+    <div className={styles.login}>
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="登录账号"
+          name="username"
+          rules={[{ required: true, message: '请输入正确的用户名' }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="登录密码"
+          name="password"
+          rules={[{ required: true, message: '请输入正确的密码' }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
+          <Checkbox>同意测试协议</Checkbox>
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+
   );
 }
 export default Login
